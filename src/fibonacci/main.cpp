0a7193e3cd84a5a9f3113da9c99af77c165f537e
Dooho Youm <dhyoum@gmail.com>
2018-12-04 08:35:03 +0900

    update fibonacci

---
+++ src/fibonacci/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int fibonacci(int n)
+{
+    if (n==0)
+        return 0;
+    else if (n==1)
+        return 1;
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main()
+{
+	int N;
+    scanf("%d", &N);
+
+    printf("%d\n", fibonacci(N));
+
+    return 0;
+}
+
