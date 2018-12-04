6087a1d56c84a7926ad8f0fbe7e28153dbac41f8
Dooho Youm <dhyoum@gmail.com>
2018-12-04 09:12:22 +0900

    fibonacci

--- src/fibonacci/main.cpp
+++ src/fibonacci/main.cpp
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
+#define MAX 90
+
+int array[MAX];
+
 int fibonacci(int n)
 {
-    if (n==0)
-        return 0;
-    else if (n==1)
-        return 1;
+    if (n==0) return 0;
+    if (n==1) return 1;
 
-    return fibonacci(n-1) + fibonacci(n-2);
+    if (array[n]) return array[n];
+    array[n] = fibonacci(n-1) + fibonacci(n-2);
+
+    return array[n];
 }
 
 int main()
 {
 	int N;
     scanf("%d", &N);
-
     printf("%d\n", fibonacci(N));
 
     return 0;
