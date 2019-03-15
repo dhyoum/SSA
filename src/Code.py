9ccd98905966ab33a57a0251b2e3a0c0767fed2f
dhyoum <dhyoum@gmail.com>
2019-03-15 23:54:42 +0900

    Update Code.py

--- src/Code.py
+++ src/Code.py
@@ -11,3 +11,27 @@ def bsearch(arr, key):
             end = mid + 1
 
     return False
+
+def lower_bound(arr, key): # key 이상인 수가 처음으로 등장하는 위치
+    start = 0
+    end = len(arr)-1
+    while start < end:
+        mid = (start + end)//2
+        if arr[mid] < key:
+            start = mid + 1
+        else:
+            end = mid
+
+    return end
+
+def upper_bound(arr, key):  # key 큰 수가 처음으로 등장하는 위치
+    start = 0
+    end = len(arr)-1
+    while start < end:
+        mid = (start + end)//2
+        if arr[mid] <= key:
+            start = mid + 1
+        else:
+            end = mid
+
+    return end
